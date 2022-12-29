86b2fc2a1c26521fdd24f39a7f0dd134ac105aeb
MSBarbieri <matheussouzabarbieri@gmail.com>
2022-12-29 12:12:19 -0300

    change start

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -1,6 +1,6 @@
 -- MODULE AUTO-START
 -- Run all the apps listed in configuration/apps.lua as run_on_start_up only once when awesome start
-
+local utils = require('utils')
 local awful = require('awful')
 
 local function run_auto_start(opts)
@@ -10,14 +10,7 @@ local function run_auto_start(opts)
       cmd = cmd()
     end
 
-    local findme = cmd
-    local firstspace = cmd:find(' ')
-    if firstspace then
-      findme = cmd:sub(0, firstspace - 1)
-    end
-
-    local exec = string.format('pgrep -u $USER -x %s > /dev/null || (%s)', findme, cmd)
-    awful.spawn.easy_async_with_shell(exec)
+    awful.spawn.with_shell(cmd)
   end
 
   for _, app in ipairs(opts.startup_scripts) do
--- rc.lua
+++ rc.lua
@@ -1,5 +1,6 @@
 require("awful.autofocus")
 require("error_handler")
+local utils = require('utils')
 
 require('theme').setup({
   wallpaper = "/home/matheus-barbieri/Pictures/wallpapers/wallpaper"
@@ -14,26 +15,25 @@ require('layout').setup({
 
 require("module").setup({
   startup_scripts = {
-    { cmd = 'picom --config=$HOME/.config/picom/picom.conf --experimental-backends' },
+    { cmd = 'picom --config=' ..os.getenv('HOME') .. '/.config/picom/picom.conf' },
     { cmd = 'spotify' },
-    { cmd = 'discord' },
-    { cmd = 'fluent_reader' },
+    {  cmd = 'discord' },
+    { cmd = 'fluent-reader' },
   },
   lazy_scripts = {
-    { cmd = os.getenv('HOME') .. '/.local/scripts/start_animated_background' },
+
+    { cmd = 'glava -d' },
     { cmd = function()
       return require('config').settings.apps.editor
     end },
-    -- { cmd = 'while [ -n "$(playerctl play -p spotify 2>&1 >/dev/null)" ]; do sleep 1; done' },
+    { cmd = 'while [ -n "$(playerctl play -p spotify 2>&1 >/dev/null)" ]; do sleep 1; done' },
   }
 })
 
 require("config").setup({
   apps = {
-    browser = 'brave-browser',
+    browser = 'brave',
     launcher = os.getenv('HOME') .. "/.config/rofi/launchers/type-4/launcher.sh",
-    editor = 'kitty ' ..
-        os.getenv('HOME') .. '/.local/bin/tmux-sessionizer ' ..
-        os.getenv('HOME'),
+    editor = 'kitty tmux'
   },
 })
